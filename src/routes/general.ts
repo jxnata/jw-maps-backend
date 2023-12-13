eefede99637a640171e6853e3f15691d55f4e33d
Jonatã Oliveira <jonata.96ol@gmail.com>
2023-12-13 20:17:12 -0300

    return cities in resume route

--- src/routes/general.ts
+++ src/routes/general.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express'
 import authUser from '../middleware/authUser'
 import Assignments from '../models/assignments'
+import Cities from '../models/cities'
 import Maps from '../models/maps'
 import Publishers from '../models/publishers'
 
@@ -12,8 +13,9 @@ export default (app: Express) => {
 			const publishers = await Publishers.count(query);
 			const maps = await Maps.count(query);
 			const assignments = await Assignments.count(query);
+			const cities = await Cities.count(query);
 
-			res.json({ publishers, maps, assignments });
+			res.json({ publishers, maps, assignments, cities });
 		} catch (error) {
 			res.status(500).json({ message: 'Error to authenticate user' });
 		}
