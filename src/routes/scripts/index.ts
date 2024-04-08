574b2d8c1c4993ef774bc760cc2a44402b88d38d
Jonatã Oliveira <jonata.96ol@gmail.com>
2024-04-08 00:38:03 -0300

    feat: ✨ wake-up route

--- src/routes/scripts/index.ts
+++ src/routes/scripts/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import backup from "./backup";
 import restore from "./restore";
+import wake_up from "./wake-up";
 
 const router = Router();
 
 router.use(backup);
 router.use(restore);
+router.use(wake_up);
 
 export default router;
