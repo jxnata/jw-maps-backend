8c0b7e3d0856468ab70de9fabc90ecec9694bded
jxnata <jxnata@proton.me>
2024-07-11 08:27:10 -0300

    fix: 🐞 format identation

--- src/routes/maps/districts.ts
+++ src/routes/maps/districts.ts
@@ -7,48 +7,48 @@ import IMap from "../../models/maps/types";
 const router = Router();
 
 router.get("/districts", authUser, async (req, res) => {
-    try {
-        let query: FilterQuery<IMap> = req.isMaster ? {} : { congregation: req.user?.congregation };
+	try {
+		let query: FilterQuery<IMap> = req.isMaster ? {} : { congregation: req.user?.congregation };
 
-        const districts = await Maps.aggregate([
-            {
-                $match: query,
-            },
-            {
-                $group: {
-                    _id: "$city",
-                    districts: { $addToSet: "$district" },
-                },
-            },
-            {
-                $lookup: {
-                    from: "cities",
-                    localField: "_id",
-                    foreignField: "_id",
-                    as: "city",
-                },
-            },
-            {
-                $unwind: "$city",
-            },
-            {
-                $project: {
-                    _id: 0,
-                    city_id: "$city._id",
-                    districts: 1,
-                },
-            },
-        ]);
+		const districts = await Maps.aggregate([
+			{
+				$match: query,
+			},
+			{
+				$group: {
+					_id: "$city",
+					districts: { $addToSet: "$district" },
+				},
+			},
+			{
+				$lookup: {
+					from: "cities",
+					localField: "_id",
+					foreignField: "_id",
+					as: "city",
+				},
+			},
+			{
+				$unwind: "$city",
+			},
+			{
+				$project: {
+					_id: 0,
+					city_id: "$city._id",
+					districts: 1,
+				},
+			},
+		]);
 
-        const response = districts.reduce((acc, { city_id, districts }) => {
-            acc[city_id] = districts;
-            return acc;
-        }, {});
+		const response = districts.reduce((acc, { city_id, districts }) => {
+			acc[city_id] = districts;
+			return acc;
+		}, {});
 
-        res.json({ districts: response });
-    } catch (error) {
-        res.status(500).json({ message: "Failed to list districts due to an internal server error." });
-    }
+		res.json({ districts: response });
+	} catch (error) {
+		res.status(500).json({ message: "Failed to list districts due to an internal server error." });
+	}
 });
 
 export default router;
